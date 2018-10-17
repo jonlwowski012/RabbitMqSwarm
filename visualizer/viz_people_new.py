085921258d83af073caf26fc60ee72a0bb83f052
jonlwowski012 <jonathan.lwowski@gmail.com>
2018-10-16 22:24:54 -0500

    refactored viz people

---
+++ visualizer/viz_people_new.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+
+# Title: Viz People
+# Description: Reads people from RabbbitMQ and visualizes it
+# Engineer: Jonathan Lwowski 
+# Email: jonathan.lwowski@gmail.com
+# Lab: Autonomous Controls Lab, The University of Texas at San Antonio
+
+
+#########          Libraries         ###################
+import numpy as np
+import random
+import pika
+import time
+import matplotlib.pyplot as plt
+import yaml
+import json
+
+### Read config parameters for RabbitMQ
+with open('config.yaml') as f:
+	config = yaml.safe_load(f)
+	hostname = config['hostname']
+	username = config['username']
+	password = config['password']
+	port = config['port']
+credentials = pika.PlainCredentials(username, password)
+
+# Receive messages from UAVs and plot
+def callback(ch, method, properties, body):
+	person = json.loads(body.decode('utf-8'))
+	plt.scatter(float(person['x_position']),float(person['y_position']))
+	plt.draw()
+	plt.pause(0.001)
+	
+	
+	'''lobal poses
+	if "START" in str(body):
+		poses = []
+	elif "END" in str(body):
+		for pose in poses:
+			plt.scatter(pose[0],pose[1],c='b',s=30)
+		plt.ylim([-300,300])
+		plt.xlim([-300,300])
+		plt.draw()
+		plt.pause(0.001)
+	else:
+		body_temp = str(body).replace("(","").replace(")","").replace("b","")
+		body_temp = body_temp.replace("'","")
+		x = float(body_temp.split(',')[0])
+		y = float(body_temp.split(',')[1])
+		poses.append([x,y])
+	#print("Len Poses: ", len(poses))'''
+
+
+if __name__ == '__main__':
+	# Establish incoming connection from UAVs
+	plt.draw()
+	plt.pause(0.01)
+	plt.ylim([-300,300])
+	plt.xlim([-300,300])
+	connection_in = pika.BlockingConnection(pika.ConnectionParameters(host=hostname, credentials=credentials, port=port))
+	channel_in = connection_in.channel()
+	channel_in.exchange_declare(exchange='people_found', exchange_type='direct')
+	result_in = channel_in.queue_declare(exclusive=True)
+	queue_in_name = result_in.method.queue
+	channel_in.queue_bind(exchange='people_found',queue=queue_in_name,routing_key='key_people_found')
+
+	# Indicate queue readiness
+	print(' [*] Waiting for messages. To exit, press CTRL+C')
+
+	# Consumption configuration
+	channel_in.basic_consume(callback,queue=queue_in_name,no_ack=False)
+
+	# Begin consuming from UAVs
+	channel_in.start_consuming()
+	
+
+
