b3a8e3c6d229fe7e70382c2512e8f2cf9d1fc1a5
Jonathan <jonathan.lwowski@gmail.com>
2018-10-29 22:15:34 -0500

    fixed dockerfiles

--- build.sh
+++ build.sh
@@ -2,12 +2,18 @@
 
 sudo docker build -t asv_client asv_client
 sudo docker build -t auctioning auctioning
-sudo docker build -t clustering clustering
+#sudo docker build -t clustering clustering
+sudo docker build -t speed_clustering speed_clustering
 sudo docker build -t metaclustering metaclustering
 sudo docker build -t tsp tsp
 sudo docker build -t uav_client uav_client
 
-
+sudo docker build -t auction_saver sql_savers/auction_saver
+sudo docker build -t boat_info_saver sql_savers/boat_info_saver
+sudo docker build -t metaclusters_saver sql_savers/metaclusters_saver
+sudo docker build -t people_saver sql_savers/people_saver
+sudo docker build -t speed_clusters_saver sql_savers/speed_clusters_saver
+sudo docker build -t tsp_saver sql_savers/tsp_saver
 
 
 
--- run.sh
+++ run.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
+xterm -e sudo docker run -it --name auction_saver auction_saver &
+xterm -e sudo docker run -it --name boat_info_saver boat_info_saver &
+xterm -e sudo docker run -it --name metaclusters_saver metaclusters_saver &
+xterm -e sudo docker run -it --name people_saver people_saver &
+xterm -e sudo docker run -it --name speed_clusters_saver speed_clusters_saver &
+xterm -e sudo docker run -it --name tsp_saver tsp_saver &
+sleep 5
+
 xterm -e sudo docker run -it --name auctioning auctioning &
-xterm -e sudo docker run -it --name clustering clustering &
+#xterm -e sudo docker run -it --name clustering clustering &
+xterm -e sudo docker run -it --name speed_clustering speed_clustering &
 xterm -e sudo docker run -it --name metaclustering metaclustering & 
 xterm -e sudo docker run -it --name tsp tsp &
-sleep 3
+sleep 5
+
 xterm -e sudo docker run -it --name uav_client uav_client &
 xterm -e sudo docker run -it --name asv_client asv_client 
