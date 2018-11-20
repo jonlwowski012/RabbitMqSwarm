3457fa3aaed3e8f70de46c8ad10f23f8bb4c7200
Your Name <you@example.com>
2018-11-20 07:11:36 -0800

    added run uav script

---
+++ run_uav.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+UAVS=10
+for i in `seq 1 $UAVS`;
+do
+	xterm -e sudo docker run -it --name uav_client_$i uav_client &
+	echo uav_client_$i
+done
+#xterm -e sudo docker run -it --name asv_client2 asv_client &
+#xterm -e sudo docker run -it --name asv_client3 asv_client &
+#xterm -e sudo docker run -it --name asv_client4 asv_client &
+#xterm -e sudo docker run -it --name asv_client5 asv_client 
