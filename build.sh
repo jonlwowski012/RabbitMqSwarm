56ef89f32d86bf9c10b0ae345787a74e808d7b02
jonlwowski012 <jonathan.lwowski@gmail.com>
2019-02-20 15:30:34 -0600

    updating docker configs

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 sudo docker build -t asv_client asv_client
 sudo docker build -t auctioning auctioning
-#sudo docker build -t clustering clustering
+sudo docker build -t clustering clustering
 sudo docker build -t speed_clustering speed_clustering
 sudo docker build -t metaclustering metaclustering
 sudo docker build -t tsp tsp
--- run.sh
+++ run.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-#xterm -e sudo docker run -it --name auction_saver auction_saver &
-#xterm -e sudo docker run -it --name boat_info_saver boat_info_saver &
-#xterm -e sudo docker run -it --name metaclusters_saver metaclusters_saver &
-#xterm -e sudo docker run -it --name people_saver people_saver &
-#xterm -e sudo docker run -it --name speed_clusters_saver speed_clusters_saver &
-#xterm -e sudo docker run -it --name tsp_saver tsp_saver &
-#sleep 5
+xterm -e sudo docker run -it --name auction_saver auction_saver &
+xterm -e sudo docker run -it --name boat_info_saver boat_info_saver &
+xterm -e sudo docker run -it --name metaclusters_saver metaclusters_saver &
+xterm -e sudo docker run -it --name people_saver people_saver &
+xterm -e sudo docker run -it --name speed_clusters_saver speed_clusters_saver &
+xterm -e sudo docker run -it --name tsp_saver tsp_saver &
+sleep 5
 
-#xterm -e sudo docker run -it --name auctioning auctioning &
-#xterm -e sudo docker run -it --name clustering clustering &
-#xterm -e sudo docker run -it --name speed_clustering speed_clustering &
-#xterm -e sudo docker run -it --name metaclustering metaclustering & 
-#xterm -e sudo docker run -it --name tsp tsp &
-#sleep 5
+xterm -e sudo docker run -it --name auctioning auctioning &
+xterm -e sudo docker run -it --name clustering clustering &
+xterm -e sudo docker run -it --name speed_clustering speed_clustering &
+xterm -e sudo docker run -it --name metaclustering metaclustering & 
+xterm -e sudo docker run -it --name tsp tsp &
+sleep 5
 
-xterm -e sudo docker run -it --name uav_client uav_client &
-xterm -e sudo docker run -it --name asv_client1 asv_client &
-xterm -e sudo docker run -it --name asv_client2 asv_client &
-xterm -e sudo docker run -it --name asv_client3 asv_client &
-xterm -e sudo docker run -it --name asv_client4 asv_client &
-xterm -e sudo docker run -it --name asv_client5 asv_client 
+#xterm -e sudo docker run -it --name uav_client uav_client &
+#xterm -e sudo docker run -it --name asv_client1 asv_client &
+#xterm -e sudo docker run -it --name asv_client2 asv_client &
+#xterm -e sudo docker run -it --name asv_client3 asv_client &
+#xterm -e sudo docker run -it --name asv_client4 asv_client &
+#xterm -e sudo docker run -it --name asv_client5 asv_client 
